c349b6ed0936fe531ae68f934b45a296f9eae1d4
k022g0011@g.neec.ac.jp <k022g0011@g.neec.ac.jp>
2024-01-12 17:37:33 +0900

    BlockGraphicsPiplineの修正

--- Engine/GraphicsPipelines/BlockGraphicsPipeline/BlockGraphicsPipeline.cpp
+++ Engine/GraphicsPipelines/BlockGraphicsPipeline/BlockGraphicsPipeline.cpp
@@ -184,7 +184,7 @@ void BlockGraphicsPipeline::InitializePSO()
 	// Depthの機能を有効化する
 	depthStencilDesc.DepthEnable = true;
 	// 書き込みします
-	depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
+	depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
 	// 比較関数はLessEqual。つまり、近ければ描画される
 	depthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
 
